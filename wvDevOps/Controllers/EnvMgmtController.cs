b75f460ec77b988921a55a475fb5c66c4c3d253d
Jim Baldwin <jbaldwin@worldventures.com>
2017-03-08 09:57:44 -0600

    initial commit

---
+++ wvDevOps/Controllers/EnvMgmtController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using wvDevOps.Helpers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace wvDevOps.Controllers
+{
+    public class EnvMgmtController : Controller
+    {
+        // GET: EnvMgmt
+        public ActionResult Index()
+        {
+            ConsulWV myconsul = new ConsulWV();
+
+            bool x = myconsul.putConsul("Dummy", "QA Environment").GetAwaiter().GetResult();
+
+            return Content(x.ToString());
+            //return View();
+        }
+    }
+}
---
+++ wvDevOps/Helpers/ConsulWV.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using Consul;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace wvDevOps.Helpers
+{
+    public class ConsulWV
+    {
+        public string consulUrl { get; set; }
+        private static ConsulClient consulClient;
+
+        public ConsulWV()
+        {
+            Uri consulUrl = new System.Uri(WebConfigurationManager.AppSettings["Consul"]);
+            var clientConfig = new ConsulClientConfiguration()
+            {
+                Address = consulUrl
+            };
+
+            consulClient = new ConsulClient(clientConfig);
+        }
+
+       public async Task<bool> putConsul(string key, string value)
+        {
+            var putPair = new KVPair(key)
+            {
+                Value = Encoding.UTF8.GetBytes(value)
+            };
+
+            var putAttempt = await consulClient.KV.Put(putPair);
+            if (putAttempt.Response)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool PutPairs(string key, string value)
+        {
+            bool x = putConsul(key, value).GetAwaiter().GetResult();
+
+            return x; 
+        }
+    }
+}
